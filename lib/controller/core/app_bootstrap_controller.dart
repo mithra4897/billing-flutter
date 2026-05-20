d8b7bca87a2f1398a8bb4cd46a10e4dc650122cb
Gokul M <gokulm@sakthicontroller.com>
2026-05-20 12:56:59 +0530

    app bootstrap logic moved to controller

---
+++ lib/controller/core/app_bootstrap_controller.dart
@@ -0,0 +1,75 @@
+import '../../screen.dart';
+
+class AppBootstrapController extends GetxController {
+  AppBootstrapController({required this.redirectTo});
+
+  final PublicBrandingService _brandingService = PublicBrandingService();
+  final AuthService _authService = AuthService();
+
+  final String redirectTo;
+
+  bool isLoading = true;
+  String? errorMessage;
+
+  @override
+  void onInit() {
+    super.onInit();
+    bootstrap();
+  }
+
+  Future<void> bootstrap() async {
+    isLoading = true;
+    errorMessage = null;
+    update();
+
+    try {
+      await _brandingService.fetchBranding();
+      final restoredSession = await AppSessionService.instance.bootstrap();
+
+      final navigator = appNavigatorKey.currentState;
+      if (navigator == null) {
+        _showError('Unable to start the application right now.');
+        return;
+      }
+
+      if (!restoredSession) {
+        navigator.pushReplacementNamed(loginRoute());
+        return;
+      }
+
+      navigator.pushReplacementNamed(redirectTo);
+      unawaited(_refreshSessionInBackground());
+    } on ApiException catch (error) {
+      _showError(error.message);
+    } catch (_) {
+      _showError('Unable to start the application right now.');
+    }
+  }
+
+  Future<void> _refreshSessionInBackground() async {
+    try {
+      final me = await _authService.me();
+      if (me.success && me.data != null) {
+        await AppSessionService.instance.updateCurrentUser(me.data!);
+      }
+      await AppSessionService.instance.refreshUserAccess();
+    } on ApiException catch (error) {
+      if (error.statusCode == 401 || error.statusCode == 403) {
+        await AppSessionService.instance.clearSession();
+      }
+    } catch (_) {}
+  }
+
+  String loginRoute() {
+    return Uri(
+      path: '/login',
+      queryParameters: <String, String>{'redirect': redirectTo},
+    ).toString();
+  }
+
+  void _showError(String message) {
+    isLoading = false;
+    errorMessage = message;
+    update();
+  }
+}
--- lib/view/core/app_bootstrap_page.dart
+++ lib/view/core/app_bootstrap_page.dart
@@ -1,3 +1,4 @@
+import '../../controller/core/app_bootstrap_controller.dart';
 import '../../screen.dart';
 
 class AppBootstrapPage extends StatefulWidget {
@@ -10,91 +11,35 @@ class AppBootstrapPage extends StatefulWidget {
 }
 
 class _AppBootstrapPageState extends State<AppBootstrapPage> {
-  final PublicBrandingService _brandingService = PublicBrandingService();
-  final AuthService _authService = AuthService();
-  bool _isLoading = true;
-  String? _errorMessage;
+  late final String _controllerTag;
 
   @override
   void initState() {
     super.initState();
-    _bootstrap();
-  }
-
-  Future<void> _bootstrap() async {
-    if (mounted) {
-      setState(() {
-        _isLoading = true;
-        _errorMessage = null;
-      });
-    }
-
-    try {
-      await _brandingService.fetchBranding();
-      final restoredSession = await AppSessionService.instance.bootstrap();
-
-      if (!mounted) {
-        return;
-      }
-
-      if (!restoredSession) {
-        Navigator.of(context).pushReplacementNamed(_loginRoute());
-        return;
-      }
-
-      Navigator.of(context).pushReplacementNamed(widget.redirectTo);
-      unawaited(_refreshSessionInBackground());
-    } on ApiException catch (error) {
-      _showError(error.message);
-    } catch (_) {
-      _showError('Unable to start the application right now.');
-    }
-  }
-
-  Future<void> _refreshSessionInBackground() async {
-    try {
-      final me = await _authService.me();
-      if (me.success && me.data != null) {
-        await AppSessionService.instance.updateCurrentUser(me.data!);
-      }
-      await AppSessionService.instance.refreshUserAccess();
-    } on ApiException catch (error) {
-      if (error.statusCode == 401 || error.statusCode == 403) {
-        await AppSessionService.instance.clearSession();
-      }
-    } catch (_) {}
-  }
-
-  String _loginRoute() {
-    return Uri(
-      path: '/login',
-      queryParameters: <String, String>{'redirect': widget.redirectTo},
-    ).toString();
-  }
-
-  void _showError(String message) {
-    if (!mounted) {
-      return;
-    }
-
-    setState(() {
-      _isLoading = false;
-      _errorMessage = message;
-    });
+    _controllerTag = persistentControllerTag('AppBootstrapController');
+    Get.put(
+      AppBootstrapController(redirectTo: widget.redirectTo),
+      tag: _controllerTag,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _isLoading
-          ? const AppLoadingView(message: 'Starting application...')
-          : _errorMessage != null
-          ? AppErrorStateView(
-              title: 'Server Unavailable',
-              message: _errorMessage!,
-              onRetry: _bootstrap,
-            )
-          : const AppLoadingView(message: 'Starting application...'),
+    return GetBuilder<AppBootstrapController>(
+      tag: _controllerTag,
+      builder: (controller) {
+        return Scaffold(
+          body: controller.isLoading
+              ? const AppLoadingView(message: 'Starting application...')
+              : controller.errorMessage != null
+              ? AppErrorStateView(
+                  title: 'Server Unavailable',
+                  message: controller.errorMessage!,
+                  onRetry: controller.bootstrap,
+                )
+              : const AppLoadingView(message: 'Starting application...'),
+        );
+      },
     );
   }
 }
