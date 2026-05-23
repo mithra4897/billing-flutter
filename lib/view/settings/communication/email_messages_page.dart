74c04860cd3d4b09421c7d830f4bc3de3db4ca5c
Siva M <siva@sakthicontroller.com>
2026-05-23 16:12:27 +0530

    email composer inline form implemented

--- lib/view/settings/communication/email_messages_page.dart
+++ lib/view/settings/communication/email_messages_page.dart
@@ -17,6 +17,17 @@ class EmailMessagesPage extends StatefulWidget {
 
 class _EmailMessagesPageState extends State<EmailMessagesPage> {
   late final String _controllerTag;
+  late final TextEditingController _moduleController;
+  late final TextEditingController _documentTypeController;
+  late final TextEditingController _documentIdController;
+  late final TextEditingController _eventCodeController;
+  late final TextEditingController _toController;
+  late final TextEditingController _ccController;
+  late final TextEditingController _bccController;
+  late final TextEditingController _subjectController;
+  late final TextEditingController _bodyController;
+  late final ValueNotifier<bool> _isHtmlNotifier;
+  final GlobalKey<FormState> _composerFormKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -24,7 +35,32 @@ class _EmailMessagesPageState extends State<EmailMessagesPage> {
     _controllerTag = persistentControllerTag(
       'EmailMessagesManagementController',
     );
+    _moduleController = TextEditingController();
+    _documentTypeController = TextEditingController();
+    _documentIdController = TextEditingController();
+    _eventCodeController = TextEditingController();
+    _toController = TextEditingController();
+    _ccController = TextEditingController();
+    _bccController = TextEditingController();
+    _subjectController = TextEditingController();
+    _bodyController = TextEditingController();
+    _isHtmlNotifier = ValueNotifier<bool>(true);
     Get.put(EmailMessagesManagementController(), tag: _controllerTag);
+  }
+
+  @override
+  void dispose() {
+    _moduleController.dispose();
+    _documentTypeController.dispose();
+    _documentIdController.dispose();
+    _eventCodeController.dispose();
+    _toController.dispose();
+    _ccController.dispose();
+    _bccController.dispose();
+    _subjectController.dispose();
+    _bodyController.dispose();
+    _isHtmlNotifier.dispose();
+    super.dispose();
   }
 
   String get _pageTitle =>
@@ -34,17 +70,27 @@ class _EmailMessagesPageState extends State<EmailMessagesPage> {
     BuildContext pageContext,
     EmailMessagesManagementController controller,
   ) async {
-    final moduleController = TextEditingController();
-    final documentTypeController = TextEditingController();
-    final documentIdController = TextEditingController();
-    final eventCodeController = TextEditingController();
-    final toController = TextEditingController();
-    final ccController = TextEditingController();
-    final bccController = TextEditingController();
-    final subjectController = TextEditingController();
-    final bodyController = TextEditingController();
-    final isHtmlNotifier = ValueNotifier<bool>(true);
     final formKey = GlobalKey<FormState>();
+    final moduleController = TextEditingController(
+      text: _moduleController.text,
+    );
+    final documentTypeController = TextEditingController(
+      text: _documentTypeController.text,
+    );
+    final documentIdController = TextEditingController(
+      text: _documentIdController.text,
+    );
+    final eventCodeController = TextEditingController(
+      text: _eventCodeController.text,
+    );
+    final toController = TextEditingController(text: _toController.text);
+    final ccController = TextEditingController(text: _ccController.text);
+    final bccController = TextEditingController(text: _bccController.text);
+    final subjectController = TextEditingController(
+      text: _subjectController.text,
+    );
+    final bodyController = TextEditingController(text: _bodyController.text);
+    final isHtmlNotifier = ValueNotifier<bool>(_isHtmlNotifier.value);
 
     final dialogContext = appNavigatorKey.currentContext ?? pageContext;
 
@@ -138,6 +184,18 @@ class _EmailMessagesPageState extends State<EmailMessagesPage> {
                 }
 
                 Navigator.of(context).pop(true);
+                _copyComposerValues(
+                  moduleController: moduleController,
+                  documentTypeController: documentTypeController,
+                  documentIdController: documentIdController,
+                  eventCodeController: eventCodeController,
+                  toController: toController,
+                  ccController: ccController,
+                  bccController: bccController,
+                  subjectController: subjectController,
+                  bodyController: bodyController,
+                  isHtml: isHtmlNotifier.value,
+                );
                 await controller.sendEmail(
                   context: pageContext,
                   module: moduleController.text,
@@ -176,31 +234,123 @@ class _EmailMessagesPageState extends State<EmailMessagesPage> {
     }
   }
 
+  void _copyComposerValues({
+    required TextEditingController moduleController,
+    required TextEditingController documentTypeController,
+    required TextEditingController documentIdController,
+    required TextEditingController eventCodeController,
+    required TextEditingController toController,
+    required TextEditingController ccController,
+    required TextEditingController bccController,
+    required TextEditingController subjectController,
+    required TextEditingController bodyController,
+    required bool isHtml,
+  }) {
+    _moduleController.text = moduleController.text;
+    _documentTypeController.text = documentTypeController.text;
+    _documentIdController.text = documentIdController.text;
+    _eventCodeController.text = eventCodeController.text;
+    _toController.text = toController.text;
+    _ccController.text = ccController.text;
+    _bccController.text = bccController.text;
+    _subjectController.text = subjectController.text;
+    _bodyController.text = bodyController.text;
+    _isHtmlNotifier.value = isHtml;
+  }
+
+  Future<void> _sendInlineEmail(
+    BuildContext context,
+    EmailMessagesManagementController controller,
+  ) async {
+    if (!_composerFormKey.currentState!.validate()) {
+      return;
+    }
+
+    await controller.sendEmail(
+      context: context,
+      module: _moduleController.text,
+      documentType: _documentTypeController.text,
+      documentId: _documentIdController.text,
+      eventCode: _eventCodeController.text,
+      to: _toController.text,
+      cc: _ccController.text,
+      bcc: _bccController.text,
+      subject: _subjectController.text,
+      body: _bodyController.text,
+      isHtml: _isHtmlNotifier.value,
+    );
+  }
+
+  Widget _buildComposerFields(EmailMessagesManagementController controller) {
+    return SettingsFormWrap(
+      children: [
+        AppFormTextField(
+          labelText: 'Module',
+          controller: _moduleController,
+          validator: Validators.required('Module'),
+        ),
+        AppDropdownField<String>.fromMapped(
+          labelText: 'Document Type',
+          mappedItems: controller.documentTypeItems,
+          initialValue: _documentTypeController.text,
+          onChanged: (value) => _documentTypeController.text = value ?? '',
+        ),
+        AppFormTextField(
+          labelText: 'Document Id',
+          controller: _documentIdController,
+          keyboardType: TextInputType.number,
+        ),
+        AppFormTextField(
+          labelText: 'Event Code',
+          controller: _eventCodeController,
+        ),
+        AppFormTextField(
+          labelText: 'To',
+          controller: _toController,
+          validator: Validators.required('To'),
+        ),
+        AppFormTextField(labelText: 'CC', controller: _ccController),
+        AppFormTextField(labelText: 'BCC', controller: _bccController),
+        AppFormTextField(
+          labelText: 'Subject',
+          controller: _subjectController,
+          validator: Validators.required('Subject'),
+        ),
+        AppFormTextField(
+          labelText: 'Body',
+          controller: _bodyController,
+          maxLines: 10,
+          validator: Validators.required('Body'),
+        ),
+        ValueListenableBuilder<bool>(
+          valueListenable: _isHtmlNotifier,
+          builder: (context, isHtml, _) {
+            return AppSwitchTile(
+              label: 'HTML Email',
+              value: isHtml,
+              onChanged: (value) => _isHtmlNotifier.value = value,
+            );
+          },
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return GetBuilder<EmailMessagesManagementController>(
       tag: _controllerTag,
       builder: (controller) {
-        if (widget.openSendComposerOnInit &&
-            !controller.initialLoading &&
-            !controller.composerLaunched) {
-          controller.markComposerLaunched();
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            if (mounted) {
-              _openSendDialog(context, controller);
-            }
-          });
-        }
-
         final content = _buildContent(context, controller);
         final actions = <Widget>[
-          AdaptiveShellActionButton(
-            onPressed: controller.sending
-                ? null
-                : () => _openSendDialog(context, controller),
-            icon: Icons.send_outlined,
-            label: 'Send Email',
-          ),
+          if (!widget.openSendComposerOnInit)
+            AdaptiveShellActionButton(
+              onPressed: controller.sending
+                  ? null
+                  : () => _openSendDialog(context, controller),
+              icon: Icons.send_outlined,
+              label: 'Send Email',
+            ),
         ];
 
         if (widget.embedded) {
@@ -234,26 +384,37 @@ class _EmailMessagesPageState extends State<EmailMessagesPage> {
     }
 
     if (widget.openSendComposerOnInit) {
-      return AppSectionCard(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Send Email', style: Theme.of(context).textTheme.titleLarge),
-            const SizedBox(height: AppUiConstants.spacingSm),
-            Text(
-              'Compose and send an email from this route-first page.',
-              style: Theme.of(context).textTheme.bodyMedium,
-            ),
-            const SizedBox(height: AppUiConstants.spacingLg),
-            AppActionButton(
-              onPressed: controller.sending
-                  ? null
-                  : () => _openSendDialog(context, controller),
-              icon: Icons.send_outlined,
-              label: 'Open Composer',
-              busy: controller.sending,
-            ),
-          ],
+      return SingleChildScrollView(
+        controller: controller.pageScrollController,
+        child: AppSectionCard(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Send Email', style: Theme.of(context).textTheme.titleLarge),
+              const SizedBox(height: AppUiConstants.spacingSm),
+              Text(
+                'Compose and send an email from this dedicated page.',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              const SizedBox(height: AppUiConstants.spacingLg),
+              Form(
+                key: _composerFormKey,
+                child: _buildComposerFields(controller),
+              ),
+              const SizedBox(height: AppUiConstants.spacingLg),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: AppActionButton(
+                  onPressed: controller.sending
+                      ? null
+                      : () => _sendInlineEmail(context, controller),
+                  icon: Icons.send_outlined,
+                  label: 'Send Email',
+                  busy: controller.sending,
+                ),
+              ),
+            ],
+          ),
         ),
       );
     }
